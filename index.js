ca0efd69da08f924d4d6474c9ddedc842a7ab472
Csaba Kiss <csakis@lanl.gov>
2018-01-04 12:40:24 -0700

    added marko as view engine.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Hapi = require('hapi')
+const Marko = require('marko')
 const Path = require('path')
 const Inert = require('inert')
 const Vision = require('vision')
@@ -14,7 +15,38 @@ const server = Hapi.server({
 
 const start = async() => {
     await server.register(Inert)
-    //await server.register(Vision)
+    await server.register(Vision)
+
+    server.views({
+        relativeTo:__dirname,
+        engines: {
+            marko: {
+                compile: (src, options) => {
+                    const opts = {
+                        preserveWhitespace: true,
+                        writeToDisk: false
+                    };
+                    const template = Marko.load(options.filename, opts);
+                    return (context) => {
+                        return template.renderToString(context);
+                    };
+                }
+            }
+        },
+        isCached: false,
+       // layout: true,
+      //  partialsPath: 'partials',
+      //  helpersPath: 'helpers',
+       path: 'views'
+    }) //end views
+
+    server.route({
+        path: '/',
+        method: 'GET',
+        handler:  (req, h) =>{
+            return h.view('index', {title: "Marko!!"})
+        }
+    })
 
     //serving static files
     server.route({
@@ -25,7 +57,9 @@ const start = async() => {
                 path: '.'
             }
         }
-    })
+    }) //end route
+
+
     await server.start()
     console.log("Hapi server started on %s", server.info.uri)
 }
