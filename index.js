75531221c561fb42ef0a1c522b908f4868e809d5
Csaba Kiss <csakis@gmail.com>
2018-01-07 13:11:30 -0700

    added cookie auth-strategy

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const Marko = require('marko')
 const Path = require('path')
 const Inert = require('inert')
 const Vision = require('vision')
+const Bcrypt = require('bcrypt')
+const HapiAuthCookie = require('hapi-auth-cookie')
+const AuthStrategy = require('./config/auth-strategy')
+const Users = require('./data/users')
 
-require('marko/node-require').install()
-
+require('marko/node-require')
 
 const logOptions = {
     ops: {
@@ -22,7 +25,7 @@ const logOptions = {
         }, {
             module: 'good-console'
         }, 'stdout'],
-      
+
         myHTTPReporter: [{
             module: 'good-squeeze',
             name: 'Squeeze',
@@ -34,24 +37,28 @@ const logOptions = {
 };
 
 
-
 const server = Hapi.server({
     port: 3000,
     routes: {
         files: {
             relativeTo: Path.join(__dirname, 'public')
         }
+    },
+    debug: {
+        request: ['*'],
+        log: ['*']
     }
-})
+});
 
 const start = async() => {
     await server.register(Inert)
     await server.register(Vision)
+    await server.register(HapiAuthCookie)
     await server.register({
         plugin: require('good'),
         logOptions
     })
-
+    //This is only needed if I want to change the view hengine to handlebars
     // server.views({
     //     relativeTo: Path.join(__dirname, 'views'),
     //     engines: {
@@ -63,8 +70,11 @@ const start = async() => {
     //     helpersPath: 'helpers'
     // })
 
+    server.auth.strategy('basic', 'cookie', AuthStrategy);
+    server.auth.default({strategy: 'basic', mode: 'try'});
+
     server.views({
-        relativeTo:__dirname,
+        relativeTo: __dirname,
         engines: {
             marko: {
                 compile: (src, options) => {
@@ -79,22 +89,58 @@ const start = async() => {
                 }
             }
         },
-        //isCached: false,
-       // layout: true,
-      //  partialsPath: 'partials',
-      //  helpersPath: 'helpers',
-       path: 'templates'
+        path: 'templates',
+        context : (request) => {
+            return {
+                user: request.auth.credentials
+            };
+
+        }
     }) //end views
 
     server.route({
         path: '/',
         method: 'GET',
+        options:{
+            auth:{
+                strategy: 'basic',
+                mode: 'required'
+            }
+        },
         handler: (req, h) => {
             return h.view('index', {
                 title: "Marko!!"
             })
         }
     })
+
+    server.route({
+        path: '/login',
+        method: 'GET',
+        handler: (req, h) => {
+            return h.view('login');
+        }
+    });
+
+    server.route({
+        path: '/login',
+        method: 'POST',
+        handler: async (req, h) => {
+            const {
+                username,
+                password
+            } = req.payload;
+            const user = Users[username];
+            if (!user || !await Bcrypt.compare(password, user.password)) {
+                console.log("Hacker alert!");
+                return h.redirect('/login');
+            }
+            req.cookieAuth.set({
+                username
+            })
+            return h.redirect('/');
+        }
+    });
 
     //serving static files
     server.route({
@@ -106,7 +152,6 @@ const start = async() => {
             }
         }
     }) //end route
-
 
     await server.start()
     console.log("Hapi server started on %s", server.info.uri)
